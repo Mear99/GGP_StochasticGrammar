1553cfc0e4539de594ad53543ad6a4767c47513d
Robbe Mahieu <robbe.mahieu@student.kuleuven.be>
2022-12-26 17:15:17 +0100

    Added the stochastic grammar project

---
+++ StochasticGrammar/StochasticGrammar.cpp
@@ -0,0 +1,9 @@
+// StochasticGrammar.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Robbe Mahieu
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Stochastic grammar demo\n";
+}
